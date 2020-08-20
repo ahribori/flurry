53c47eab436b0bf384225b81f2c058eb88f10fc0
Daniel <ahribori@gmail.com>
2020-08-21 00:38:28 +0900

    UPDATE home ui

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,11 +1,14 @@
 import 'dart:convert';
 
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flurry/model/todo.model.dart';
 import 'package:flurry/provider/todo.provider.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
+import 'package:supercharged/supercharged.dart';
 
 class Home extends StatelessWidget {
   fetchTodo(BuildContext context) async {
@@ -20,37 +23,128 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final double statusBarHeight = MediaQuery.of(context).padding.top;
+
     return Scaffold(
-        body: ListView(
-      children: [
-        Container(
-            height: 50,
-            child: const Center(
-              child: Text('Hello, World!'),
-            )),
-        RaisedButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/login');
-          },
+      backgroundColor: '#5a4fef'.toColor(),
+      bottomNavigationBar: CurvedNavigationBar(
+        height: 60,
+        animationDuration: Duration(milliseconds: 200),
+        animationCurve: Curves.easeInOut,
+        backgroundColor: '#5a4fef'.toColor(),
+        items: <Widget>[
+          Icon(Icons.check_box_outline_blank, size: 30),
+          Icon(Icons.check_box, size: 30),
+          Icon(Icons.settings, size: 30),
+        ],
+        onTap: (index) {
+          //Handle button tap
+        },
+      ),
+      body: Container(
+        child: ListView(
+          padding: EdgeInsets.only(
+              top: statusBarHeight + 8, right: 8, bottom: 8, left: 8),
+          children: [
+            Card(
+              child: InkWell(
+                splashColor: Colors.deepPurpleAccent.withAlpha(30),
+                onTap: () {
+                  print('Card tapped.');
+                },
+                child: Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.symmetric(vertical: 16, horizontal: 8),
+                  child: Row(
+                    children: [
+                      Expanded(
+                          child: const ListTile(
+                        title: Text(
+                          '마트에서 장보기',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        subtitle: Text('2020년 7월 8일 오후 3시'),
+                      )),
+                      Container(
+                        margin: EdgeInsets.only(right: 8),
+                        child: IconButton(
+                          iconSize: 28,
+                          icon: Icon(Icons.check_box_outline_blank),
+                          onPressed: () {},
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              child: InkWell(
+                splashColor: Colors.deepPurpleAccent.withAlpha(30),
+                onTap: () {
+                  print('Card tapped.');
+                },
+                child: Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.symmetric(vertical: 16, horizontal: 8),
+                  child: Row(
+                    children: [
+                      Expanded(
+                          child: const ListTile(
+                        title: Text(
+                          '블로그에 글쓰기',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        subtitle: Text('2020년 7월 8일 오후 3시'),
+                      )),
+                      Container(
+                        margin: EdgeInsets.only(right: 8),
+                        child: IconButton(
+                          iconSize: 28,
+                          icon: Icon(Icons.check_box_outline_blank),
+                          onPressed: () {},
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              child: InkWell(
+                splashColor: Colors.deepPurpleAccent.withAlpha(30),
+                onTap: () {
+                  print('Card tapped.');
+                },
+                child: Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.symmetric(vertical: 16, horizontal: 8),
+                  child: Row(
+                    children: [
+                      Expanded(
+                          child: const ListTile(
+                        title: Text(
+                          '잠자기',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        subtitle: Text('2020년 7월 8일 오후 3시'),
+                      )),
+                      Container(
+                        margin: EdgeInsets.only(right: 8),
+                        child: IconButton(
+                          iconSize: 28,
+                          icon: Icon(Icons.check_box_outline_blank),
+                          onPressed: () {},
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
         ),
-        RaisedButton(
-          onPressed: () {
-            this.fetchTodo(context);
-          },
-          child: Text('fetch'),
-        ),
-        Text('정현승 이라고 합니다. My Name is Daniel.',
-            style: TextStyle(
-              fontSize: 30,
-            )),
-        ListBody(
-          children: context
-              .watch<TodoProvider>()
-              .todoList
-              .map((e) => Text(e.title))
-              .toList(),
-        )
-      ],
-    ));
+      ),
+    );
   }
 }
